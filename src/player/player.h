5aa95c1ea1d7c78142e12d965b07871ed32c8b4d
Henrik Hautakoski <henrik@fiktivkod.org>
2010-12-10 17:13:10 +0100

    added license.

--- src/player/player.h
+++ src/player/player.h
@@ -1,3 +1,15 @@
+/* player.h - Base class for a player
+ *
+ *   Copyright (C) 2009-2010  Henrik Hautakoski <henrik@fiktivkod.org>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef PLAYER_H
 #define PLAYER_H
--- src/square.h
+++ src/square.h
@@ -1,3 +1,15 @@
+/* square.h - Represents a square on the board
+ *
+ *   Copyright (C) 2009-2010  Henrik Hautakoski <henrik@fiktivkod.org>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef SQUARE_H
 #define SQUARE_H
