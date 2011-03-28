5e529917d7de729e322cbf344779104aaac5af31
Henrik Hautakoski <henrik@fiktivkod.org>
2011-03-28 08:31:13 +0200

    Engine/Graphics: adding Texture class.

---
+++ src/Engine/Graphics/Texture.h
@@ -0,0 +1,32 @@
+
+#ifndef ENGINE_GRAPHICS_TEXTURE_H
+#define ENGINE_GRAPHICS_TEXTURE_H
+
+#include "internal.h"
+
+class Texture {
+
+private :
+    GLuint id;
+    int width;
+    int height;
+    std::string filename;
+    
+public :
+	Texture();
+    Texture(std::string filename);
+	~Texture();
+	
+	int getWidth() { return width; };
+	int getHeight() { return height; };
+	
+	void Load(std::string filename);
+	
+    void Reload();
+    
+    void Delete();
+    
+    void Draw(GLint x, GLint y);
+};
+
+#endif /* ENGINE_GRAPHICS_TEXTURE_H */
---
+++ src/Engine/Graphics/internal.h
@@ -0,0 +1,12 @@
+
+#ifndef GRAPHICS_INTERNAL_H
+#define GRAPHICS_INTERNAL_H
+
+#include <string>
+#include <iostream>
+
+#include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_opengl.h"
+
+#endif /* GRAPHICS_INTERNAL_H */
