22f96f36467ef88211c00db38436f64cb9fb50de
Henrik Hautakoski <henrik@fiktivkod.org>
2011-07-26 14:00:13 +0200

    board: remove display function

--- src/board.cc
+++ src/board.cc
@@ -10,7 +10,7 @@
  *   You should have received a copy of the GNU General Public License
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
 #include <iostream>
 #include "board.h"
 
@@ -32,21 +32,21 @@ bool Board::validSquare(int position)
 		cerr << "ERROR: invalid square index " << position << endl;
 		return false;
 	}
-	
+
 	return true;
 }
 
-int Board::numberOfPieces(int piece_type) 
+int Board::numberOfPieces(int piece_type)
 {
 	int i, m = 0;
-	
+
 	for(i=0; i < N_SQUARES; i++) {
-		
+
 		// count this square if matched
 		if (square[i].hasPiece(piece_type) && ++m == 3)
 			break;
 	}
-	
+
 	return m;
 }
 
@@ -62,7 +62,7 @@ void Board::delPiece(int position)
 {
     if (!validSquare(position))
 		return;
-		
+
 	square[position].setFree();
 }
 
@@ -70,7 +70,7 @@ bool Board::isFree(int position)
 {
 	if (!validSquare(position))
 		return false;
-	
+
 	return square[position].isFree();
 }
 
@@ -78,7 +78,7 @@ bool Board::hasPiece(int position, int piece_type)
 {
 	if (!validSquare(position))
 		return false;
-		
+
 	return square[position].hasPiece(piece_type);
 }
 
@@ -86,19 +86,19 @@ bool Board::hasCounterPiece(int position, int piece_type)
 {
 	if (!validSquare(position))
 		return false;
-	
+
 	return !square[position].isFree() && !square[position].hasPiece(piece_type);
 }
 
-bool Board::checkPattern(int piece_type) 
+bool Board::checkPattern(int piece_type)
 {
 	int i, j;
-	
+
 	for(i=0; i < 3; i++) {
 		for(j=0; j < 3; j++) {
 			if (!square[(i*3)+j].hasPiece(piece_type))
 				break;
-			
+
 			if (j == 2)
 				return true;
 		}
@@ -106,48 +106,29 @@ bool Board::checkPattern(int piece_type)
 		for(j=0; j < 3; j++) {
 			if (!square[i+(j*3)].hasPiece(piece_type))
 				break;
-			
+
 			if (j == 2)
 				return true;
 		}
 	}
-	
+
 	/* Diagonal */
-	
+
 	for(i=0; i < 3; i++) {
 		if (!square[4*i].hasPiece(piece_type))
 			break;
-		
+
 		if (i == 2)
 			return true;
 	}
-	
+
 	for(i=1; i <= 3; i++) {
 		if (!square[2*i].hasPiece(piece_type))
 			break;
-			
+
 		if (i == 3)
 			return true;
 	}
-	
+
 	return false;
-}
-
-void Board::display() 
-{
-	/* Draw beginning line */
-	cout << "+---+---+---+" << endl;
-
-	/* For each square */
-	for(int i=0; i < N_SQUARES; i++) {
-		
-		/* Draw square */
-		cout << "| " << square[i].getMarker() << " ";
-		
-		/* For every third square, close it and draw line-break */
-		if ((i+1) % 3 == 0) {
-			cout << "|" << endl 
-			     << "+---+---+---+" << endl;
-	    }
-	}
 }
